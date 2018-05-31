1945a821681f2e0093a4effcda9de1447aeef35f
Ian Brown <bigianb@gmail.com>
2018-05-31 21:16:51 +0100

    added ORA test

--- source/cpu/m6502_test.cpp
+++ source/cpu/m6502_test.cpp
@@ -19,3 +19,26 @@ BOOST_AUTO_TEST_CASE(test_reset)
     BOOST_CHECK_EQUAL(cpu.regPC, 0x0123);
 }
 
+BOOST_AUTO_TEST_CASE(test_opcodes)
+{
+	Ram ram(64 * 1024);
+	DirectAddressBus bus(ram);
+
+	for (int i = 0; i < 0xFFFF; ++i) {
+		ram.bytes[i] = 0;
+	}
+
+	m6502 cpu(bus);
+	cpu.reset();
+
+	ram.bytes[0x0123] = 0x01;		// ORA ($nn,X)
+	ram.bytes[0x0124] = 0x10;
+
+	ram.bytes[0x11] = 0x13;
+
+	cpu.regA = 0x20;
+	cpu.regX = 1;
+	cpu.regPC = 0x0123;
+	cpu.step();
+	BOOST_CHECK_EQUAL(cpu.regA, 0x33);
+}
